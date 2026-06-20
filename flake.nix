1ca8e09b6bea90f4215ac3ccb387dbede7e54703
Ara Hacopian <ara@hacopian.de>
2026-06-20 14:30:40 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "A Nix-flake-based Rust development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              overlays = [ rust-overlay.overlays.default ];
+            }
+          )
+        );
+    in
+    {
+      devShells = forAllSystems (
+        pkgs:
+        let
+          rust = pkgs.rust-bin.stable.latest.default.override {
+            extensions = [
+              "rust-src"
+              "rust-analyzer"
+              "clippy"
+              "rustfmt"
+            ];
+          };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [
+              rust
+              pkgs.bacon
+              pkgs.cargo-edit
+              pkgs.cargo-nextest
+              pkgs.just
+            ];
+
+            RUST_BACKTRACE = 1;
+          };
+        }
+      );
+
+      formatter = forAllSystems (pkgs: pkgs.nixfmt);
+    };
+}
